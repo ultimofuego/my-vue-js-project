eb61f39b900e68b69a7f13386267a4b24818cda9
ultimofuego <rakualex@gmail.com>
2021-06-21 15:25:12 +0400

    final

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -5,22 +5,24 @@
           <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
           This date already exists
         </div>
-        <div class="form-group ">
-          <label for="date">Select the date</label>
-          <input v-model="rowData.date" class="input_field" type="date" id="userId" placeholder="Date" required>
-        </div>
-        <div class="form-group">
-          <label for="rateRUB">Type RUB rate</label>
-          <input v-model.number="rowData.rateRUB" class="input_field" type="text" id="rate" placeholder="Rate" required>
-        </div>
-        <div class="form-group">
-          <label for="rateUSD">Type USD rate</label>
-          <input v-model.number="rowData.rateUSD" class="input_field" type="text" id="rate" placeholder="Rate" required>
-        </div>
-        <div class="form-group">
-          <label for="rateEUR">Type EUR rate</label>
-          <input v-model.number="rowData.rateEUR" class="input_field" type="text" id="rate" placeholder="Rate" required>
-        </div>
+        
+          <div class="form-group ">
+            <label for="date">Select the date</label>
+            <input v-model="rowData.date" class="input_field" type="date" id="userId" placeholder="Date" required>
+          </div>
+          <div class="form-group">
+            <label for="rateRUB">Type RUB rate</label>
+            <input v-model.number="rowData.rateRUB" class="input_field" type="text" id="rate" placeholder="Rate" required>
+          </div>
+          <div class="form-group">
+            <label for="rateUSD">Type USD rate</label>
+            <input v-model.number="rowData.rateUSD" class="input_field" type="text" id="rate" placeholder="Rate" required>
+          </div>
+          <div class="form-group">
+            <label for="rateEUR">Type EUR rate</label>
+            <input v-model.number="rowData.rateEUR" class="input_field" type="text" id="rate" placeholder="Rate" required>
+          </div>
+        
         <button type="submit" class="submitButton" @click="check">Add row</button>
       </form>
 </template>
